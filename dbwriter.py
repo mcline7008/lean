6aa1c03f2901d8a826631e829383a64d7fcd38ff
Matthew Cline <mcline7008@gmail.com>
2016-04-06 19:49:01 -0400

    added json parsing code and eliminated the db connection testing code

--- dbwriter.py
+++ dbwriter.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python
 # coding: utf-8
 
+import json
+import pprint from pprint
 import MySQLdb as mdb
 import sys
+
+dataFile = open("data.json")
+data = json.load(dataFile)
+
+podID = data["pod_id"]
+mac = data["mac_address"]
+ip = data["ip_address"]
+time = data["time"]
+temp = data["temp"]
+luminosity = data["luminosity"]
+acceleration = data["acceleration"]
+latitude = data["latitude"]
+longitude = data["longitude"]
 
 con = mdb.connect('localhost', 'leanuser', 'leanteam', 'leanSensors')
 cur = con.cursor()
 
-cur.execute("SHOW TABLES")
-
-tables = cur.fetchall()
-
-for table in tables:
-	print table
-	
