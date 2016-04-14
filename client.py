46c1054f6890189073f46c12fe74c369ff32b8b9
Justin Scothorn <scothorn.2@wright.edu>
2016-04-14 08:59:01 -0400

    Added files via upload
    Added continuous loop to client script

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import socket
 import demjson
 
@@ -5,26 +7,16 @@ TCP_IP = '10.1.71.92'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = {"reading_type": "GPS", "pod_id": "1", "mac_address": "fe:fe:fe:fe:fe", "ip_address": "192.168.1.9", "time": "2016-04-13 18:45:30", "temp": "98", "luminosity": "85", "acceleration": "1.2", "latitude": "29.3499123", "longitude": "43.1234567"}
+
+
 json = demjson.encode(MESSAGE)
-
-#begin sensor test while loop
-
-#sensor test collect readings on time intervals
-#on each update create the message for the reading
-#encode the json packet and complete the send proccess
-
-#update time
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(json)
-data = s.recv(BUFFER_SIZE)
+
+while 1:
+	s.send(json)
+	data = s.recv(BUFFER_SIZE)
+	print "Send data: ", data
 s.close()
 
-<<<<<<< HEAD
-print("Send data: ", data)
-=======
-print "Send data: ", data
 
-#end of while loop
->>>>>>> 002d38ec258f39f8c0dcb503cadd354932ff3f86
