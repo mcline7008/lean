0b96db560a7c8d73a4dc14f6db8dda036e7af530
mcline7008 <r.matthew.cline@gmail.com>
2016-04-14 08:45:20 -0400

    Changed the json object to encode a dictionary instead of a list

--- client.py
+++ client.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
-
 import socket
 import demjson
 
-TCP_IP = '192.168.1.9'
+TCP_IP = '10.1.71.92'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = [{"reading_type": "GPS", "pod_id": "1", "mac_address": "fe:fe:fe:fe:fe", "ip_address": "192.168.1.9", "time": "2016-04-13 18:45:30", "temp": "98", "luminosity": "85", "acceleration": "1.2", "latitude": "29.3499123", "longitude": "43.1234567"}]
+MESSAGE = {"reading_type": "GPS", "pod_id": "1", "mac_address": "fe:fe:fe:fe:fe", "ip_address": "192.168.1.9", "time": "2016-04-13 18:45:30", "temp": "98", "luminosity": "85", "acceleration": "1.2", "latitude": "29.3499123", "longitude": "43.1234567"}
 json = demjson.encode(MESSAGE)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,4 +13,4 @@ s.send(json)
 data = s.recv(BUFFER_SIZE)
 s.close()
 
-print "Send data: ", data
+print("Send data: ", data)
